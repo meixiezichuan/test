a423e25ff9f8608ea320028fba74d9df90f02dfe
Ubuntu <azureuser@liqo-test.lbovr2qqdlyutnt1ssrs21q43c.gx.internal.cloudapp.net>
2024-05-17 09:32:45 +0000

    update server

--- tcp-server/main.go
+++ tcp-server/main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func handleClient(conn net.Conn) {
 	defer conn.Close()
 
 	// Read data from the client
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err)
-		return
-	}
-	fmt.Printf("Received data: %s\n", buf[:n])
+        reader := bufio.NewReader(conn)
+	for {
+	    data, err := reader.ReadString('\n')
+	    if err != nil {
+	    	fmt.Println("Error reading:", err)
+	    	return
+	    }
+	    fmt.Printf("Received data: %s\n", data)
 
-	// Send response back to the client
-	conn.Write([]byte("Hello from server\n"))
+	    // Send response back to the client
+	    conn.Write([]byte("Hello from server\n"))
+	}
 }
 
 func main() {
 	// Start TCP server on port 8080
 	port := os.Args[1]
 	lport := ":"+ port
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	listener, err := net.Listen("tcp", lport)
 	if err != nil {
 		fmt.Println("Error listening:", err)
@@ -44,5 +53,6 @@ func main() {
 		fmt.Println("Accepted connection from:", conn.RemoteAddr())
 		go handleClient(conn) // Handle client connection concurrently
 	}
+	<-sigs
 }
 
