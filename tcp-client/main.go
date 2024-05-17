f7714061c2fc285daebbccbfe1e5aedb6d3d834a
meixiezichuan <meixiezichuan@163.com>
2024-05-17 20:51:27 +0800

    update to calc latency

--- tcp-client/main.go
+++ tcp-client/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
 	"net"
@@ -10,7 +9,7 @@ import (
 )
 
 func randomInRange(min, max int) int {
-    return rand.Intn(max-min+1) + min
+	return rand.Intn(max-min+1) + min
 }
 
 func main() {
@@ -21,9 +20,9 @@ func main() {
 
 	serverAddr := os.Args[1]
 	rand.Seed(time.Now().UnixNano())
-	min := 40000
-        max := 48000
-        sourcePort := randomInRange(min, max)
+	min := 1024
+	max := 48000
+	sourcePort := randomInRange(min, max)
 	localAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", sourcePort))
 	if err != nil {
 		return
@@ -41,50 +40,18 @@ func main() {
 	}
 	defer conn.Close()
 
-	message := fmt.Sprintf("hello from %s\n", localAddr.String())
-	// Send data to server
-	_, err = conn.Write([]byte(message))
-	if err != nil {
-		fmt.Println("Error sending data:", err)
-		return
-	}
-	fmt.Println("Sent:", message)
-        startTime := time.Now()
-        data := make([]byte, 1024) // 1KB message
-        for i := 0; i < 1023; i++ {
-            data[i] = 'a'
-        }
-
-        // 最后一个元素赋值为 '\n'
-        data[1023] = '\n'
-        //for i := 0; i < 10000; i++ { // Send 10MB of data
-        //    _, err = conn.Write(data)
-        //    if err != nil {
-        //        log.Fatalf("Error sending data: %v", err)
-        //    }
-        //}
-
 	// Read response from server
-	reader := bufio.NewReader(conn)
+	//reader := bufio.NewReader(conn)
 	for {
-	     _, err = conn.Write([]byte("hello\n"))
-            if err != nil {
-                    fmt.Println("Error sending data:", err)
-                    return
-            }
-	    fmt.Println("Sent: hello")
-
-            // Set a deadline for reading from the connection
-            conn.SetReadDeadline(time.Now().Add(60 * time.Second)) // 5 seconds timeout
-
-            // Wait and read the response
-            response, err := reader.ReadString('\n')
-            if err != nil {
-                    fmt.Println("Error read data:", err)
-            }
-	    fmt.Println("Received:", response)
+		startTime := time.Now()
+		//fmt.Println("clientTime:", startTime.String())
+		mTime := startTime.Format(time.RFC3339) + "\n"
+		_, err = conn.Write([]byte(mTime))
+		if err != nil {
+			fmt.Println("Error sending data:", err)
+			return
+		}
+		time.Sleep(10 * time.Millisecond)
 	}
-        duration := time.Since(startTime)
-        fmt.Println("Sent 10MB of data in %v", duration)
-}
 
+}
--- tcp-server/main.go
+++ tcp-server/main.go
@@ -6,33 +6,42 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 )
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
 	defer conn.Close()
 
 	// Read data from the client
-        reader := bufio.NewReader(conn)
+	reader := bufio.NewReader(conn)
 	for {
-	    data, err := reader.ReadString('\n')
-	    if err != nil {
-	    	fmt.Println("Error reading:", err)
-	    	return
-	    }
-	    fmt.Printf("Received data: %s\n", data)
-
-	    // Send response back to the client
-	    conn.Write([]byte("Hello from server\n"))
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+		now := time.Now()
+		mtime := data[:len(data)-1]
+		clientTime, err := time.Parse(time.RFC3339, string(mtime))
+		if err != nil {
+			fmt.Println("Error parsing timestamp:", err)
+			return
+		}
+		dur := now.Sub(clientTime)
+		fmt.Println("latency:", dur.Milliseconds())
 	}
 }
 
 func main() {
 	// Start TCP server on port 8080
 	port := os.Args[1]
-	lport := ":"+ port
+	lport := ":" + port
 
 	sigs := make(chan os.Signal, 1)
+
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	listener, err := net.Listen("tcp", lport)
@@ -40,9 +49,9 @@ func main() {
 		fmt.Println("Error listening:", err)
 		return
 	}
-	defer listener.Close()
-	fmt.Println("Server listening on port 6666")
 
+	fmt.Println("Server listening on port 6666")
+	var wg sync.WaitGroup
 	// Accept and handle incoming connections
 	for {
 		conn, err := listener.Accept()
@@ -50,9 +59,12 @@ func main() {
 			fmt.Println("Error accepting connection:", err)
 			continue
 		}
-		fmt.Println("Accepted connection from:", conn.RemoteAddr())
-		go handleClient(conn) // Handle client connection concurrently
+		//fmt.Println("Accepted connection from:", conn.RemoteAddr())
+		wg.Add(1)
+		go handleClient(conn, &wg) // Handle client connection concurrently
 	}
 	<-sigs
+	listener.Close()
+	wg.Wait()
+	fmt.Println("exit..")
 }
-
