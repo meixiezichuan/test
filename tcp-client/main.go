5f7ba13f1e8a7fe3ed78249d141f7b22ea3d6d36
meixiezichuan <meixiezichuan@163.com>
2024-05-18 00:17:50 +0800

    client write 5 minutes

--- tcp-client/main.go
+++ tcp-client/main.go
@@ -20,21 +20,22 @@ func main() {
 	}
 
 	serverAddr := os.Args[1]
-	rand.Seed(time.Now().UnixNano())
-	min := 1024
-	max := 48000
-	sourcePort := randomInRange(min, max)
-	localAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", sourcePort))
-	if err != nil {
-		return
-	}
-
-	// Create a dialer with the local address
-	dialer := &net.Dialer{
-		LocalAddr: localAddr,
-	}
-	// Connect to TCP server
-	conn, err := dialer.Dial("tcp", serverAddr)
+	//rand.Seed(time.Now().UnixNano())
+	//min := 1024
+	//max := 48000
+	//sourcePort := randomInRange(min, max)
+	//localAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf(":%d", sourcePort))
+	//if err != nil {
+	//	return
+	//}
+	//
+	//// Create a dialer with the local address
+	//dialer := &net.Dialer{
+	//	LocalAddr: localAddr,
+	//}
+	//// Connect to TCP server
+	//conn, err := dialer.Dial("tcp", serverAddr)
+	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		fmt.Println("Error connecting:", err)
 		return
@@ -43,7 +44,7 @@ func main() {
 
 	// Read response from server
 	//reader := bufio.NewReader(conn)
-	for {
+	for i := 0; i < 30000; i++ {
 		startTime := time.Now().UnixNano()
 		//fmt.Println("clientTime:", startTime)
 		mTime := strconv.FormatInt(startTime, 10) + "\n"
